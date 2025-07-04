ce4ea7c5d8c2de641ec1823dd2b272cd989d7170
achraf1934 <achraf.zarroug96@gmail.com>
2025-07-04 01:46:56 +0100

    vercel json 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist', // Spécifie le dossier de sortie
+    assetsDir: 'assets'
   }
 })
